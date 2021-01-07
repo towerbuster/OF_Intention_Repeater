8493e04087b38270da26453a16f24eca322f0aa1
towerbuster <77104716+towerbuster@users.noreply.github.com>
2021-01-07 15:47:59 +0100

    Update of_intent_repeater_001.cpp

--- of_intent_repeater_001.cpp
+++ of_intent_repeater_001.cpp
@@ -1,13 +1,6 @@
 /******************************************************************************
 Can be run online at : https://www.onlinegdb.com/online_c++_compiler
-*******************************************************************************/
-
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+Also : https://repl.it/languages/cpp
 *******************************************************************************/
 
 #include <iostream>
