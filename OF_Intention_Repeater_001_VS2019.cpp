d5d187020bee9efd74b28ab5f73d47d51a32df79
towerbuster <77104716+towerbuster@users.noreply.github.com>
2021-01-08 11:27:44 +0100

    Windows version
    Windows Version

--- OF_Intention_Repeater_001_VS2019.cpp
+++ OF_Intention_Repeater_001_VS2019.cpp
@@ -37,9 +37,6 @@ int main()
 {
     signal(SIGINT, signal_callback_handler);
     signal(SIGTERM, signal_callback_handler);
-    //signal(SIGKILL, signal_callback_handler);
-    //signal(SIGSTOP, signal_callback_handler);
-    //signal(SIGTSTP, signal_callback_handler);
 
     std::string intention, process_intention;
 
