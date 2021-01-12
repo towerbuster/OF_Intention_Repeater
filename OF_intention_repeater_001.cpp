47efd9c5f5694d28b35114561865f4ac1cd1764e
towerbuster <77104716+towerbuster@users.noreply.github.com>
2021-01-12 18:18:19 +0100

    Update OF_intention_repeater_001.cpp

--- OF_intention_repeater_001.cpp
+++ OF_intention_repeater_001.cpp
@@ -14,12 +14,12 @@ using namespace std;
 
 unsigned long intention_repeats_counter=0; unsigned int M=0;
 
-std::chrono::_V2::system_clock::time_point t1;
-std::chrono::_V2::system_clock::time_point t2;
+std::chrono::_V2::steady_clock::time_point t1;
+std::chrono::_V2::steady_clock::time_point t2;
 
 // When CTRL+C (SIGINT), this is executed, also when program is stopped (SIGTERM, eg. useful on repl.it but not on onlinegdb)
 void signal_callback_handler(int signum) {
-    t2 = std::chrono::high_resolution_clock::now();
+    t2 = std::chrono::steady_clock::now();
     std::chrono::duration<double> diff = t2-t1;
     std::cout << "Total execution time " << diff.count() << " s\n";
     unsigned long average_intention_repeats_per_sec = (unsigned long)(intention_repeats_counter/diff.count());
@@ -41,7 +41,7 @@ int main()
 
     std::string intention, process_intention;
     
-    t1 = std::chrono::high_resolution_clock::now();
+    t1 = std::chrono::steady_clock::now();
     intention = "LOVE";
     while(true) {
         process_intention = intention;
